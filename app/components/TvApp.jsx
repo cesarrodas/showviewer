16cd577e26381df3c2d0e2bb5aefb7bb2125d69e
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-18 11:41:24 -0500

    created tvapp component

---
+++ app/components/TvApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class TvApp extends React.Component {
+	render(){
+		return (
+			<div className="container">
+				<h1>This is the TV Viewer</h1>
+			</div>
+		);
+	}
+};
