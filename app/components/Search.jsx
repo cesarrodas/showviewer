d4f5c2d0707df5e718fe790a1d674db8bae7e827
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-20 23:44:23 -0500

    added the search functionality and state

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 
 export class Search extends React.Component {
-	constructor(){
-		super();
-		this.state = {
-			searchText: ''
-		}
-	}
+	constructor(props) {
+    super(props);
+    this.state = {
+    	searchText: 'Enter a tv show'
+    };
+    this.onChange = this.onChange.bind(this);
+    this.sendSearch = this.sendSearch.bind(this);
+  }
+  onChange(){
+  	this.setState({
+  		searchText: this.refs.show.value
+  	})
+  }
+  sendSearch(event){
+  	event.preventDefault();
+  	this.props.searchingText(this.state.searchText);
+  }
 	render(){
 		return (
 			<div className="row">
 				<div className="small-8 small-centered">
+					<form ref="form" onSubmit={this.sendSearch}>
 						<div className="small-10 column">
-							<input type="text" name="search"/>
+							<input type="text" ref="show" onChange={this.onChange} value={this.state.searchText} name="search"/>
 						</div>
 						<div className="small-2 column">
-							<button className="button">Search</button>
+							<button className="button" name="submit" type="submit">Search</button>
 						</div>
+					</form>
 				</div>
 			</div>
 		)
--- app/components/TvApp.jsx
+++ app/components/TvApp.jsx
@@ -3,18 +3,28 @@ import GetShowInfo from '../utils/axios_methods';
 import { Search } from './Search';
 
 export class TvApp extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			showName: ''
+		};
+	}
 	componentDidMount(){
 		GetShowInfo('daredevil').then((data) => {
 			console.log(data);
 		});
+	}
+	search(passedName){
+		this.setState({showName: passedName});
 	}
 	render(){
 		return (
 			<div className="container">
 				<div className="small-6 small-centered">
 					<h1 className="header">Tv Show Viewer</h1>
+					<h1>{this.state.showName}</h1>
 					<br/>
-					<Search className="small-10 small-centered"></Search>
+					<Search searchingText={this.search.bind(this)}></Search>
 				</div>
 			</div>
 		);
