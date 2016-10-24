af2917141641e7380a427f6969c5123722aac5ca
Cesar Rodas <rodascesar03@yahoo.com>
2016-10-23 23:25:41 -0500

    added actor component

---
+++ app/components/Actor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Actor extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<div className="actor">
+
+			</div>
+		)
+	};
+};
--- app/components/Cast.jsx
+++ app/components/Cast.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router';
+
 import { Navigation } from './Navigation';
+import {Actor} from './Actor';
 
 export class Cast extends React.Component {
+	handleActors(){
+		var actors = this.props.allShowInfo.cast ? this.props.allShowInfo.cast : [];
+		var actorCollection = actors.map((actor, index) => <Actor key={index} info={actor}></Actor>);
+		// var actors = this.props.allShowInfo.cast;
+		// console.log("HELLO", actors);
+		return actorCollection;
+	}
 	render() {
 		return (
 			<div>
 				<Navigation selected="cast"></Navigation>
 				<br/>
 				<h1>Cast Component</h1>
+				<br/>
+				{this.handleActors()}
 			</div>
 		);
 	}
